0040babcfe8e63ee072ba9b75dca2d4dca23f281
Asparageese <60201247+Asparageese@users.noreply.github.com>
2024-05-02 22:14:50 +0100

    Update PRIMES.hs

--- douhasskill/PRIMES.hs
+++ douhasskill/PRIMES.hs
@@ -1,7 +1,4 @@
--- haskell prime number finder.
--- no parallel computations dand no statistical methods.
--- to be added in prime number finder version 2, the goal is to push
--- the search bound to 1,000,000 and easily preform computations. 
+-- unoptimized prime number finder, haskell. 
 bound = 1000
 domain = drop 1 [1,3..bound]
 mnet = [x*y | x <- domain, y <- [a | a <- [2..((length domain))]], (x*y <= last domain) && (mod (x*y) 2 /= 0)] 
