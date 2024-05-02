470ae0d608bd9258a19839a9e2e59d1164238d14
Asparageese <jeevanpb1337@gmail.com>
2024-05-02 14:51:43 +0000

    killme0

--- douhasskill/PRIMES.hs
+++ douhasskill/PRIMES.hs
@@ -1,11 +1,14 @@
 -- haskell prime number finder.
-bound = 7
-domain = drop 1 [1,3..bound] 
-z = [map (tau *) m | m<-[2..(bound/2)],tau<-[domain]]
-
-
+-- no parallel computations dand no statistical methods.
+-- to be added in prime number finder version 2, the goal is to push
+-- the search bound to 1,000,000 and easily preform computations. 
+bound = 1000
+domain = drop 1 [1,3..bound]
+mnet = [x*y | x <- domain, y <- [a | a <- [2..((length domain))]], (x*y <= last domain) && (mod (x*y) 2 /= 0)] 
+tau = [x | x <- domain, not(any (x==) mnet)]
 main :: IO()
 main = do
   print "BEGINING MAIN CALL"
   print domain
-  print z
+  print tau
+  
