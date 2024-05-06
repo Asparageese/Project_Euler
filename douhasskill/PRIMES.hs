b0c4f0c140d73b8b27da51427185d4d299593021
Asparageese <jeevanpb1337@gmail.com>
2024-05-06 21:44:41 +0100

    0

--- douhasskill/PRIMES.hs
+++ douhasskill/PRIMES.hs
@@ -1,5 +1,5 @@
 -- unoptimized prime number finder, haskell. 
-bound = 1000
+bound = 10000
 domain = drop 1 [1,3..bound]
 mnet = [x*y | x <- domain, y <- [a | a <- [2..((length domain))]], (x*y <= last domain) && (mod (x*y) 2 /= 0)] 
 tau = [x | x <- domain, not(any (x==) mnet)]
