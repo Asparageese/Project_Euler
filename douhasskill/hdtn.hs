f16e62bb5715fabd7799c34a3500999cba024010
Asparageese <jeevanpb1337@gmail.com>
2024-05-20 11:55:29 +0100

    0

--- douhasskill/hdtn.hs
+++ douhasskill/hdtn.hs
@@ -1,2 +1,13 @@
--- Simple brute force highly divisible triangle number finder.
---
+hdtn :: Int
+hdtn = 10
+yBool a = if even a then 0 else 1
+test = yBool hdtn
+hdtnF = fromIntegral hdtn::Float
+
+
+main :: IO()
+main = do
+  print "main pass"
+  print hdtn
+  print test
+  print hdtnF
