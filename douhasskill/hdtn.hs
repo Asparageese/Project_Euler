2ddf6f954d81a2d7bc243d1d6698ed3ed958e2a0
Asparageese <jeevanpb1337@gmail.com>
2024-05-21 14:53:14 +0100

    0

--- douhasskill/hdtn.hs
+++ douhasskill/hdtn.hs
@@ -1,13 +1,16 @@
 hdtn :: Int
-hdtn = 10
-yBool a = if even a then 0 else 1
-test = yBool hdtn
+hdtn = 20
 hdtnF = fromIntegral hdtn::Float
-
+yBool a = if even a then 0 else 1
+yBnds a hnum
+  | a == 0 = hnum/2.0
+  | otherwise = hnum/3.0
+fSet a = if a == 0 then [2..(yBnds(yBool hdtn) hdtnF)] else [1,3..(yBnds(yBool hdtn) hdtnF)]
+test = fSet (yBool hdtn)
 
 main :: IO()
 main = do
   print "main pass"
   print hdtn
-  print test
   print hdtnF
+  print test
